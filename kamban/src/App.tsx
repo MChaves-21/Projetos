535422503a0a0cb0af840b889578208864e90252
Murilo Chaves Oliveira <murilochaves211105@gmail.com>
2025-05-04 14:18:26 -0300

    densenvolvendo
    começando o desenvolvimento

--- kamban/src/App.tsx
+++ kamban/src/App.tsx
@@ -1,12 +1,22 @@
-import { Box, Heading } from "@radix-ui/themes"
+import { CreateTaskForm } from "./components/CreateTaskForm";
+import { TaskBoard } from "./components/TaskBoard";
+import { Box, Flex, Heading } from "@radix-ui/themes";
 
-function App(){
-  return(
-  
-  <Box>
-    <Heading>react kambam</Heading>
-  </Box>
+function App() {
+  return (
+      <Box maxWidth="80rem" mx="auto">
+        <Box height="4rem" px="4">
+          <Flex align="center" gap="4" height="100%">
+            <Heading size="8" weight="light">React Kanban</Heading>
+            <CreateTaskForm />
+          </Flex>
+        </Box>
+
+        <Box px="4" maxWidth="80rem" mx="auto">
+          <h2 className="mb-3">Quadro de tarefas</h2>
+          <TaskBoard />
+        </Box>
+      </Box>
   )
 }
-
 export default App
