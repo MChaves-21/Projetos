b2645738a4b10b5541878ec11bdb920edbba2db7
Murilo Chaves Oliveira <murilochaves211105@gmail.com>
2025-08-31 20:48:51 -0300

    Refatorando

--- Bliblioteca de jogos/Bliblioteca de Jogos/src/App.jsx
+++ Bliblioteca de jogos/Bliblioteca de Jogos/src/App.jsx
@@ -1,74 +1,26 @@
-import { useState } from 'react'; // Não se esqueça de importar o useState
+import NewGameForm from "./components/newGameForm"
+import Game from "./components/Game"
+import useGameCollection from "./hooks/useGameCollection"
 
-export default function App() {
-  const [games, setGames] = useState(()=>{
-    const storedGames = localStorage.getItem("games");
-    return storedGames ? JSON.parse(storedGames) : [];
-  });
-  const [title, setTitle] = useState("");
-  const [cover, setCover] = useState("");
-  
-  const RemoverGame = (id) => {
-  setGames(state => {
-    const newState = state.filter(game => game.id !== id);
-    localStorage.setItem("games", JSON.stringify(newState));
-    return newState;})
-  }
+function App() {
+  const { games, addGame, removeGame } = useGameCollection()
 
-
-  const addGame = ({title,cover}) => {
-    const id = Math.floor(Math.random() * 10000);
-    const newGame = {id, title, cover};
-    setGames(state => {
-      const newState = [...state, newGame];
-      localStorage.setItem("games", JSON.stringify(newState));
-      return newState;
-    });
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    addGame({ title, cover });
-    setCover("");
-    setTitle(""); 
-  }
-  
   return (
-    <div id="app">
-      <h1>Bliblioteca de jogos</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Titulo </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="cover">Capa  </label>
-          <input 
-            type="text"
-            name="cover"
-            id="cover"
-            value={cover}
-            onChange={(e) => setCover(e.target.value)}
-          />
-        </div>
-        <button type="submit">Adicionar a Bliblioteca</button>
-      </form>
-      <div className='games'>
+    <div className="app">
+      <h1>Biblioteca de Jogos</h1>
+      <NewGameForm addGame={addGame} />
+      <div className="games">
         {games.map((game) => (
-          <div key={game.id} className="game">
-            <h2>{game.title}</h2>
-              <img src={game.cover} alt={`Capa do jogo ${game.title}`} />
-            <button onClick={()=>RemoverGame(game.id)}>Remover</button>
-          
-          </div>
+          <Game
+            key={game.id}
+            title={game.title}
+            cover={game.cover}
+            onRemove={() => removeGame(game.id)}
+          />
         ))}
       </div>
     </div>
   )
 }
+
+export default App
